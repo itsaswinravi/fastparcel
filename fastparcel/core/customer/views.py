4b6dd7253cd8f1795e3765391ae87ea15cca6242
itsaswinravi <achu322001@gmail.com>
2024-05-15 11:14:49 +0530

    19

--- fastparcel/core/customer/views.py
+++ fastparcel/core/customer/views.py
@@ -5,6 +5,8 @@ from core.customer import forms
 
 
 from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 
 @login_required()
@@ -15,17 +17,31 @@ def home(request):
 @login_required(login_url='/sign-in/?next=/customer/')
 def profile_page(request):
     user_form=forms.BasicUserForm(instance=request.user)
-    customer_form=forms.BasicCustomerForm(instance=request.user.customer) 
+    customer_form=forms.BasicCustomerForm(instance=request.user.customer)
+    password_form= PasswordChangeForm(request.user)
+
     if request.method == 'POST':
+
+     if request.POST.get('action') == 'update_profile':
         user_form=forms.BasicUserForm(request.POST,instance=request.user)
         customer_form= forms.BasicCustomerForm(request.POST,request.FILES,instance=request.user.customer)
         if user_form.is_valid() and customer_form.is_valid():
             user_form.save()
             customer_form.save()
-            
+
             messages.success(request, 'Your profile has been updated')
             return redirect(reverse('customer:profile'))
+        
+        elif request.POST.get('action') =='update_password':
+            password_form = PasswordChangeForm(request.user, request.POST)
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user)
+
+                messages.success(request, 'Your password has been updated')
+                return  redirect(reverse('customer:profile'))
     return render(request, 'customer/profile.html',
                    {'user_form':user_form ,
-                    "customer_form":customer_form
+                    "customer_form":customer_form,
+                    "password_form":password_form
                     }) 
